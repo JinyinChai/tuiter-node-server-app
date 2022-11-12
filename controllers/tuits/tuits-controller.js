3cfc1b5be64c754a011de09767394c654eaff479
Jinyin Chai <cjy990324>
2022-11-12 11:26:26 -0800

    A8

--- controllers/tuits/tuits-controller.js
+++ controllers/tuits/tuits-controller.js
@@ -5,10 +5,13 @@ let tuits = posts;
 const createTuit = (req, res) => {
     const newTuit = req.body;
     newTuit._id = (new Date()).getTime()+'';
-    newTuit.likes = 0;
+    newTuit.heart = 0;
     newTuit.liked = false;
+    newTuit.dislikes = 0;
     newTuit.username = "NASA";
     newTuit.handle = "@nasa";
+    newTuit.comments = 0;
+    newTuit.tweets = 0;
     newTuit.avatarIcon = "images/elon_musk_avatar.jpeg";
     tuits.push(newTuit);
     res.json(newTuit);
@@ -17,16 +20,27 @@ const createTuit = (req, res) => {
 const findTuits = (req, res) =>
     res.json(tuits);
 
-const updateTuit = (req, res) => {
-    const tuitdIdToUpdate = req.params.tid;
-    const updates = req.body;
-    const tuitIndex = tuits.findIndex(
-        (t) => t._id.toString() === tuitdIdToUpdate)
-    tuits[tuitIndex] =
-        {...tuits[tuitIndex], ...updates};
-       //
+// const updateTuit = (req, res) => {
+//     const tuitdIdToUpdate = req.params.tid;
+//     const updates = req.body;
+//     const tuitIndex = tuits.findIndex(
+//         (t) => t._id.toString() === tuitdIdToUpdate)
+//     tuits[tuitIndex] =
+//         {...tuits[tuitIndex], ...updates};
+//        //
+//
+//     res.sendStatus(200);
+// }
 
+const updateTuit = (req, res) => {
+
+    const tuitdIdToUpdate = req.params.tid;
+
+    const updatedTuit = req.body;
+
+    tuits = tuits.map(t => t._id === tuitdIdToUpdate ? updatedTuit : t);
     res.sendStatus(200);
+
 }
 
 const deleteTuit = (req, res) => {
--- controllers/tuits/tuits.js
+++ controllers/tuits/tuits.js
@@ -1,20 +1,4 @@
 export default [
-    // {
-    //     "_id": 123, "topic": "Space", "username": "SpaceX",
-    //     "handle": "@spacex", "time": "2h", "image": "spacex.png",
-    //     "title": "SpaceX's Mission",
-    //     "tuit": "You want to wake up in the morning and think the future is going to be great - and that’s what being a spacefaring civilization is all about. It’s about believing in the future and thinking that the future will be better than the past. And I can’t think of anything more exciting than going out there and being among the stars",
-    //     "liked": true, "likes": 2345,
-    //     "replies": 123, "retuits": 432
-    // },
-    // {
-    //     "_id": 234, "topic": "Traffic", "username": "The Boring Company",
-    //     "handle": "@boringcompany", "time": "2h", "image": "boring2.jpg",
-    //     "title": "100s of SpaceX Starships land on Mars after a 6 month journey. 1000s of Martian colonists being building Mars Base 1",
-    //     "tuit": "The Boring Company (TBC) is an American infrastructure and tunnel construction services company founded by Elon Musk. Its ongoing and proposed projects are designed for intra-city ('loop') transit systems.",
-    //     "liked": true, "likes": 2345,
-    //     "replies": 123, "retuits": 432
-    // },
     {
         "_id": 123,
         "avatarIcon":"images/elon_musk_avatar.jpeg",
@@ -32,6 +16,7 @@ export default [
         "comments": 456,
         "tweets": 555,
         "heart": 222,
+        "dislikes": 34,
         "liked": true,
         "skip": false,
         "tuit": "You want to wake up in the morning and think the future is going to be great - and that’s what being a spacefaring civilization is all about. It’s about believing in the future and thinking that the future will be better than the past. And I can’t think of anything more exciting than going out there and being among the stars"
@@ -51,6 +36,7 @@ export default [
         "comments": 123,
         "tweets": 234,
         "heart": 345,
+        "dislikes": 23,
         "liked": false,
         "skip": true,
         "tuit": "\"tuit\": \"You want to wake up in the morning and think the future is going to be great - and that’s what being a spacefaring civilization is all about. It’s about believing in the future and thinking that the future will be better than the past. And I can’t think of anything more exciting than going out there and being among the stars\""
@@ -70,6 +56,7 @@ export default [
         "comments": 123,
         "tweets":123,
         "heart": 333,
+        "dislikes": 55,
         "liked": false,
         "skip": true,
         "tuit": "\"tuit\": \"You want to wake up in the morning and think the future is going to be great - and that’s what being a spacefaring civilization is all about. It’s about believing in the future and thinking that the future will be better than the past. And I can’t think of anything more exciting than going out there and being among the stars\""
